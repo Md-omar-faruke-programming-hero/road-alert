8a7bf8b86fda6c984f436e639c2d00917fa059b9
Md-omar-faruke-programming-hero <farukeomar@gmail.com>
2021-10-05 09:24:28 +0600

    edit

--- src/Component/Coursefee/Coursefee.js
+++ src/Component/Coursefee/Coursefee.js
@@ -33,21 +33,21 @@ const Coursefee = () => {
 
     return (
         <div>
-            <h3 className="title text-center mb-2">Course fee for Auto private car</h3>
+            <h3 className="title text-center mb-2">Course details for "Auto private car"</h3>
             <Row xs={1} md={3} className="g-1 container m-auto mb-2">
             {
                 auto.map(fee=><Autocar key={fee.name} fee={fee}></Autocar>)
             }
             </Row>
             <hr />
-            <h3 className="title text-center mb-2">Course fee for Manual private car</h3>
+            <h3 className="title text-center mb-2">Course details for "Manual private car"</h3>
             <Row xs={1} md={3} className="g-1 container m-auto mb-2">
             {
                 manual.map(fee=><Manualcar key={fee.name} fee={fee}></Manualcar>)
             }
             </Row>
             <hr />
-            <h3 className="title text-center mb-2">Course fee for Bike & Scooter</h3>
+            <h3 className="title text-center mb-2">Course details for "Bike & Scooter"</h3>
             <Row xs={1} md={3} className="g-1 container m-auto mb-2">
             {
                bikeScooter.map(fee=><BikeScooter key={fee.name} fee={fee}></BikeScooter>)
